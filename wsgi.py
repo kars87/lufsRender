a1d4cd17568de9b648f1ee97906e3e4f283a3ea5
Kim Rosvoll <66441323+kars87@users.noreply.github.com>
2025-04-06 21:01:20 +0200

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-# This file is required for PythonAnywhere
+
 import sys
 import os
 
 # Add your project directory to the Python path
-path = './app.py'
+path = 'app.py'
 if path not in sys.path:
     sys.path.append(path)
 
